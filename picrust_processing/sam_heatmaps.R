9422e1de5cc28bb53469820f7bdf83ec9e643d06
sam <64753002+samkallum@users.noreply.github.com>
2023-11-02 10:36:15 -0700

    sam picrust

--- picrust_processing/sam_heatmaps.R
+++ picrust_processing/sam_heatmaps.R
@@ -5,11 +5,12 @@ library(tibble)
 library(tidyverse)
 library(ggprism)
 library(patchwork)
+library(ggh4x)
 
 # load abundance data
 abundance_file <- "picrust_processing/picrust2_out_pipeline/pathways_out/path_abun_unstrat.tsv.gz"
 abundance_data <- read_delim(abundance_file, delim = "\t", col_names = TRUE, trim_ws = TRUE) %>% as.data.frame()
-abundance_data$pathway = rownames(abundance_data)
+#abundance_data$pathway = rownames(abundance_data)
 #abundance_data = abundance_data[,-1]
 # load metadata
 metadata <- read_delim("colombia/metadata_categorized_CL.txt",
@@ -22,7 +23,7 @@ metadata <- read_delim("colombia/metadata_categorized_CL.txt",
 # kegg_abundance <- ko2kegg_abundance("picrust_processing/picrust2_out_pipeline/KO_metagenome_out/pred_metagenome_unstrat.tsv") 
 
 # Perform pathway differential abundance analysis (DAA) using ALDEx2 method
-daa_results_df <- pathway_daa(abundance = abundance_data[,-1], 
+daa_results_df <- pathway_daa(abundance = abundance_data %>% column_to_rownames("pathway"), 
                               metadata = metadata, 
                               group = "smoker", 
                               daa_method = "LinDA", 
@@ -36,8 +37,8 @@ pathway_heatmap(abundance = abundance_data %>% filter(pathway %in% feature_with_
 # Generate pathway PCA plot
 # Please change column_to_rownames() to the feature column if you are not using example dataset
 # Please change group to "your_group_column" if you are not using example dataset
-pathway_pca(abundance = abundance_data %>% column_to_rownames("pathway"), metadata = metadata, group = "Environment")
-
+pathway_pca(abundance = abundance_data %>% column_to_rownames("pathway"), metadata = metadata, group = "smoker")
+pathway_pca(abundance = abundance_data %>% column_to_rownames("pathway"), metadata = metadata, group = "LDL_category")
 # Run pathway DAA for multiple methods
 # Please change column_to_rownames() to the feature column if you are not using example dataset
 # Please change group to "your_group_column" if you are not using example dataset
