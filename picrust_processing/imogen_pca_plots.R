3ad7976cd3e5b7fb739c95168d977c213097166a
iporter-16 <63209879+iporter-16@users.noreply.github.com>
2023-11-02 15:53:44 -0700

    looked into unexpressed reads -i
    Dead end! Nevermind

--- picrust_processing/imogen_pca_plots.R
+++ picrust_processing/imogen_pca_plots.R
@@ -1,4 +1,4 @@
-#From picrust2 analysis, produces PCA plots for smokers and nonsmokers
+#PURPOSE: From picrust2 analysis, produces PCA plots for smokers and nonsmokers
 library(ggpicrust2)
 library(tibble)
 library(tidyverse)
@@ -6,13 +6,13 @@ library(ggprism)
 library(patchwork)
 library(ggh4x)
 
-#read abundance data
+### Read abundance data
 abundance_file <- "picrust_processing/picrust2_out_pipeline/pathways_out/path_abun_unstrat.tsv"
 abundance_data <- read_delim(abundance_file, delim = "\t", col_names = TRUE, trim_ws = TRUE) %>% as.data.frame()
-#read metadata
+### Read metadata
 metadata <- read_delim("colombia/metadata_categorized_CL.txt",delim = "\t",escape_double = FALSE,trim_ws = TRUE)
 
-#isolate smoking and nonsmoking from abundance data and metadata
+### Isolate smoking and nonsmoking from abundance data and metadata
 metadata_smoking <- filter(metadata, metadata$smoker=="Yes")
 metadata_nonsmoking <- filter(metadata, metadata$smoker=="No")
 
@@ -24,7 +24,7 @@ abundance_data_nonsmoking <- select(abundance_data, metadata_nonsmoking$`#Sample
 abundance_data_nonsmoking$pathway = abundance_data$pathway
 abundance_data_nonsmoking <- abundance_data_nonsmoking %>% select(pathway, everything())
 
-#Filter data to remove 0 standard deviation
+### Filter data to remove 0 standard deviation
 row_sds_nonsmok <- abundance_data_nonsmoking %>% column_to_rownames("pathway") %>% apply(1, sd)
 filtered_LDL_nonsmok <- abundance_data_nonsmoking[row_sds_nonsmok != 0, ]
 row.names(filtered_LDL_nonsmok) <- NULL
@@ -32,7 +32,13 @@ row_sds_smok <- abundance_data_smoking %>% column_to_rownames("pathway") %>% app
 filtered_LDL_smok <- abundance_data_smoking[row_sds_smok != 0, ]
 row.names(filtered_LDL_smok) <- NULL
 
-#Create plots
+### Pulling the interesting unexpressed stuff
+interesting_nonsmok <- abundance_data_nonsmoking[row_sds_nonsmok== 0, ]
+interesting_smok <- abundance_data_smoking[row_sds_smok== 0, ]
+all_interesting <- data.frame(feature=c(interesting_nonsmok$pathway,interesting_smok$pathway))
+all_interesting <- pathway_annotation(pathway = "MetaCyc", daa_results_df = all_interesting, ko_to_kegg = FALSE)
+
+### Create plots
 PCA_total <- pathway_pca(abundance = abundance_data %>% column_to_rownames("pathway"), 
                          metadata = metadata, group = "smoker")+ggtitle("PCA plot for all participants")
 PCA_nonsmok_LDL <- pathway_pca(abundance = filtered_LDL_nonsmok %>% column_to_rownames("pathway"), 
@@ -40,7 +46,7 @@ PCA_nonsmok_LDL <- pathway_pca(abundance = filtered_LDL_nonsmok %>% column_to_ro
 PCA_smok_LDL <- pathway_pca(abundance = filtered_LDL_smok %>% column_to_rownames("pathway"), 
                             metadata = metadata_smoking, group = "LDL_category")+ggtitle("PCA plot for smokers")
 
-#Save plots. Only change wd to save plots!
-setwd("/Users/porte16049/Desktop/MICB475/micb475-project2/pcoa_plots")
-ggsave("pca_all_LDL.png",PCA_total,scale=1)
-ggsave("pca_smokers_LDL.png",PCA_smok_LDL,scale=1)
+### Save plots. Only change wd to save plots! Keep commented ut
+# setwd("/Users/porte16049/Desktop/MICB475/micb475-project2/pcoa_plots")
+# ggsave("pca_all_LDL.png",PCA_total,scale=1)
+# ggsave("pca_smokers_LDL.png",PCA_smok_LDL,scale=1)
